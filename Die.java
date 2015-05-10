38b4803f7640ddd045a68c3242e4204f5124f65a
giacalone99 <giacalone99@gmail.com>
2015-05-09 20:41:38 -0400

    Update Die.java

--- Die.java
+++ Die.java
@@ -1,11 +1,32 @@
-/**
-  * @description Die Class
-  * @author
-  * @version
-  */
-  
+/*
+ * File: Die.java
+ * Name: Giacalone/Kelly/Xue
+ * Date: 05/08/2015
+ * -------------------------
+ * This class represents a die (singular of dice).
+ */
+
 public class Die {
-  public Die() {
     
-  }
+    private int numSides;
+    private int faceValue;
+    
+    public Die() {
+        numSides = 6;
+        faceValue = roll();
+    }
+    
+    public Die(int numSides) {
+        this.numSides = numSides;
+        faceValue = roll();
+    }
+    
+    private int roll() {
+        return (int) (numSides * Math.random() + 1);
+    }
+    
+    public int getNumSides() {
+        return numSides;
+    }
+    
 }
