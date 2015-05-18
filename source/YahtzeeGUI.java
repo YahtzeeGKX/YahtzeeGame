98856c78252e8f6fe6277ec7477e6c815a90f3ad
BenKelly99 <benjaminkelly@me.com>
2015-05-18 15:57:36 -0400

    Reworked once again
    Attempts to fir Luke's description of the GUI.
    Uses BorderLayout as opposed to GridLayout for the frame.
    Added the JTable
    Reworked the scorecard into an OOB solution
    Reworked many other changes

--- source/YahtzeeGUI.java
+++ source/YahtzeeGUI.java
@@ -1,84 +1,72 @@
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
-import java.awt.geom.Line2D;
 
 public class YahtzeeGUI extends JFrame implements ActionListener {
     private JPanel scoreButtonPanel;
     private JPanel diceButtonPanel;
-    private JPanel scorePanel;
+    private JPanel tablePanel;
+    private JPanel rollPanel;
     private JFrame frame;
-    
+
+    private JButton rollButton;
+
     private JButton[] diceButtons;
-    private JComponent[] scoreButtons;
+    private JTable table;
+
+    private PlayerCard player1, player2, currPlayer;
 
     private int[] rolls;
-    private int rollsLeft, catsLeft, totScore, uppBonus, sessionHighScore;
+    private int rollsLeft, catsLeft;
 
     private final ImageIcon[][] pics = {
             {new ImageIcon("Pictures/1.png"), new ImageIcon("Pictures/2.png"), new ImageIcon("Pictures/3.png"), 
-            new ImageIcon("Pictures/4.png"), new ImageIcon("Pictures/5.png"), new ImageIcon("Pictures/6.png")}, 
-            {new ImageIcon("Pictures/1YELL.png"), new ImageIcon("Pictures/2YELL.png"), new ImageIcon("Pictures/3YELL.png"), 
-            new ImageIcon("Pictures/4YELL.png"), new ImageIcon("Pictures/5YELL.png"), new ImageIcon("Pictures/6YELL.png")}};
-
-    Die myDie;
+                new ImageIcon("Pictures/4.png"), new ImageIcon("Pictures/5.png"), new ImageIcon("Pictures/6.png")}, 
+            {new ImageIcon("Pictures/1Y.png"), new ImageIcon("Pictures/2Y.png"), new ImageIcon("Pictures/3Y.png"), 
+                new ImageIcon("Pictures/4Y.png"), new ImageIcon("Pictures/5Y.png"), new ImageIcon("Pictures/6Y.png")}};
 
     private void newGame() {
         // creates a new panel for the buttons
         scoreButtonPanel = new JPanel();
         scoreButtonPanel.setLayout(new GridLayout(13, 1));
+
         diceButtonPanel = new JPanel();
         diceButtonPanel.setLayout(new GridLayout(1,5));
-        scorePanel = new JPanel();
-        
-        myDie = new Die();
+
+        rollPanel = new JPanel();
+
+        tablePanel = new JPanel();
+
         rolls = new int[5];
 
-        rollsLeft = 3;
-        catsLeft = 13;
-        uppBonus = 0;
-        totScore = 0;
+        player1 = new PlayerCard("A");
+        player2 = new PlayerCard("B");
+        currPlayer = player1;
 
+        rollsLeft = 3;
+
+        initTable();
         initButton();
-        
-        frame.add(diceButtonPanel);
-        frame.add(scoreButtonPanel);
-        frame.add(scorePanel);
+
+        frame.add(diceButtonPanel, BorderLayout.NORTH);
+        frame.add(scoreButtonPanel, BorderLayout.WEST);
+        frame.add(rollPanel, BorderLayout.CENTER);
+        frame.add(tablePanel, BorderLayout.EAST);
         frame.pack();
         frame.setVisible(true);
     }
-    
+
     public void createFrame() {
         // creates a new frame for the scoreboard
         frame = new JFrame("Yahtzee!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(3,1));
+        frame.setLayout(new BorderLayout());
         newGame();
     }
-    
+
     // Sets up the buttons
     private void initButton() {
-        scorePanel.add(new JLabel("Total Score: " + totScore));
-
-        scoreButtons = new JComponent[14];
         diceButtons = new JButton[5];
-
-        //Sets up all the different scoring options
-        scoreButtons[0] = new JButton("Roll Unselected Dice (" + rollsLeft + " rolls remaining)");
-        scoreButtons[1] = new JButton("Ones");
-        scoreButtons[2] = new JButton("Twos");
-        scoreButtons[3] = new JButton("Threes");
-        scoreButtons[4] = new JButton("Fours");
-        scoreButtons[5] = new JButton("Fives");
-        scoreButtons[6] = new JButton("Sixes");
-        scoreButtons[7] = new JButton("Three of a Kind");
-        scoreButtons[8] = new JButton("Four of a Kind");
-        scoreButtons[9] = new JButton("Full House");
-        scoreButtons[10] = new JButton("Small Straight");
-        scoreButtons[11] = new JButton("Large Straight");
-        scoreButtons[12] = new JButton("Chance");
-        scoreButtons[13] = new JButton("Yahtzee!");
 
         for(int i=0; i<diceButtons.length; i++){
             diceButtons[i] = new JButton();
@@ -86,50 +74,93 @@ public class YahtzeeGUI extends JFrame implements ActionListener {
             diceButtons[i].addActionListener(this);
         }
 
-        for(JComponent button: scoreButtons)
-            ((JButton)button).addActionListener(this);
+        for(int i=0; i<13; i++){
+            ((JButton)player1.getComponentAtIndex(i)).addActionListener(this);
+            ((JButton)player2.getComponentAtIndex(i)).addActionListener(this);
+        }
 
-        rollUnselected();            
+        rollButton = new JButton("Roll Unselected Dice (" + rollsLeft + " rolls remain");
+        rollButton.addActionListener(this);
 
-        update();
+        rollUnselected();
+    }
+
+    private void initTable() {
+        table = new JTable(21, 3);
+        table.setRowHeight(24);
+        table.setFont(new Font("Arial", Font.PLAIN, 14));
+        table.getColumn("A").setMinWidth(125);
+        table.getColumn("A").setMaxWidth(125);
+        table.setShowGrid(true);
+        table.setGridColor(Color.BLACK);
+        table.setEnabled(false);
+
+        table.setShowGrid(true);
+        table.setGridColor(Color.BLACK);
+        table.setEnabled(false);
+        table.setValueAt("Upper Section", 0, 0);
+        table.setValueAt("Player 1", 0, 1);
+        table.setValueAt("Player 2", 0, 2);
+        table.setValueAt("Aces", 1, 0);
+        table.setValueAt("Twos", 2, 0);
+        table.setValueAt("Threes", 3, 0);
+        table.setValueAt("Fours", 4, 0);
+        table.setValueAt("Fives", 5, 0);
+        table.setValueAt("Sixes", 6, 0);
+        table.setValueAt("Total Score", 7, 0);
+        table.setValueAt("Bonus", 8, 0);
+        table.setValueAt("", 9, 0);
+        table.setValueAt("Lower Section", 10, 0);
+        table.setValueAt("3 of a Kind", 11, 0);
+        table.setValueAt("4 of a Kind", 12, 0);
+        table.setValueAt("Full House", 13, 0);
+        table.setValueAt("Sm. Straight", 14, 0);
+        table.setValueAt("Lg. Straight", 15, 0);
+        table.setValueAt("Yahtzee!", 16, 0);
+        table.setValueAt("Chance", 17, 0);
+        table.setValueAt("Total (Lwr. Sec.)", 18, 0);
+        table.setValueAt("Total (Upr. Sec.)", 19, 0);
+        table.setValueAt("Grand Total", 20, 0);
+
+        table.repaint();
     }
 
     public void actionPerformed(ActionEvent e){
         JButton button = (JButton) e.getSource();
         if(button.getIcon() != null)
-            changeDieBackground(button);
-        else if(button == scoreButtons[0])
+            changeDieBackground(button); 
+        else if(button == rollButton)
             rollUnselected();
         else
             scoreButtonClicked(button);
     }
     // The code executed when a button is clicked
     private void scoreButtonClicked(JButton button) {
-        int score = Points.getPoints(button, rolls);
-        totScore += score;
-        if(totScore >= sessionHighScore)
-        sessionHighScore = totScore;
+        currPlayer.removeButton(currPlayer.indexOfButton(button));
         rollsLeft = 3;
-        catsLeft--;
-        for(int i=0;i<scoreButtons.length;i++){
-            if(scoreButtons[i] == button){
-                scoreButtonPanel.remove(button);
-                scoreButtons[i] = new JLabel("" + button.getText() + ": " + score);
-                update();
-            }
-        }
+
+        int col = 1;
+        if(currPlayer == player2)
+            col = 2;
+
+        for(int j=0;j<21;j++)
+            if(table.getValueAt(j, 0).equals(button.getText()))
+                table.setValueAt("" + Points.getPoints(button, rolls), j, col);
 
         for(JButton j: diceButtons)
             j.setIcon(pics[0][0]);
 
-        if(button.getText().equals("Ones") || button.getText().equals("Twos") || button.getText().equals("Threes")
-        || button.getText().equals("Fours") || button.getText().equals("Five") || button.getText().equals("Sixes"))
-            uppBonus += score;
+        if(currPlayer == player1)
+            currPlayer = player2;
+        else
+            currPlayer = player1;
 
-        if(catsLeft == 0){
-            if(uppBonus >= 63)
-                totScore += 35;
-            ((JButton)scoreButtons[0]).setText("New Game");
+        update();
+
+        if(player1.isDone() && player2.isDone()){
+            rollButton.setText("New Game");
+            findTotals();
+            update();
         }
         else
             rollUnselected();
@@ -137,57 +168,77 @@ public class YahtzeeGUI extends JFrame implements ActionListener {
 
     private void changeDieBackground(JButton button) {
         boolean found = false;
-        for(int color = 0; color < 2 && !found; color++){
-            for(int die = 0; die < pics[0].length && !found; die++){
+        for(int color = 0; color < 2 && !found; color++)
+            for(int die = 0; die < pics[0].length && !found; die++)
                 if(button.getIcon() == pics[color][die]){
                     button.setIcon(pics[Math.abs(color-1)][die]);
-                    found = true;
-                }
-            }
-        }
+                    found = true;}
 
         update();
     }
 
     private void rollUnselected(){
-        if(((JButton)scoreButtons[0]).getText().equals("New Game")){
+        if(rollButton.getText().equals("New Game")){
+            frame.remove(tablePanel);
             frame.remove(diceButtonPanel);
             frame.remove(scoreButtonPanel);
-            frame.remove(scorePanel);
+            frame.remove(rollPanel);
             newGame();
         }
         else if(rollsLeft > 0){
             rollsLeft--;
-            for(int i=0; i<diceButtons.length; i++){
+            for(int i=0; i<diceButtons.length; i++)
                 for(ImageIcon aWhiteIcon: pics[0])
-                if(diceButtons[i].getIcon() == aWhiteIcon || diceButtons[i].getIcon() == null){
-                    rolls[i] = myDie.roll();
-                    diceButtons[i].setIcon(pics[0][rolls[i]-1]);
-                }
-            }
+                    if(diceButtons[i].getIcon() == aWhiteIcon || diceButtons[i].getIcon() == null){
+                        rolls[i] = (int)((6 * Math.random()) + 1);
+                        diceButtons[i].setIcon(pics[0][rolls[i]-1]);}
             update();
         }
     }
 
     private void update(){
-        frame.remove(scorePanel);
-        
-        ((JButton)scoreButtons[0]).setText("Roll Unselected Dice (" + rollsLeft + " rolls remaining)");
+        if(!rollButton.getText().equals("New Game"))
+            rollButton.setText("Roll Unselected Dice (" + rollsLeft + " rolls remaining)");
 
-        scoreButtonPanel.removeAll();
         diceButtonPanel.removeAll();
-        scorePanel.removeAll();
+        scoreButtonPanel.removeAll();
+        rollPanel.removeAll();
+        tablePanel.removeAll();
 
-        for(JComponent compo: scoreButtons)
-            scoreButtonPanel.add(compo);
+        for(int i=0; i<13; i++)
+            scoreButtonPanel.add(currPlayer.getComponentAtIndex(i));
+
         for(JButton button: diceButtons)
             diceButtonPanel.add(button);
-        scorePanel.add(new JLabel("Total Score: " + totScore + "           High-Score: " + sessionHighScore));
+
+        rollPanel.add(rollButton);
+
+        tablePanel.add(table);
 
         diceButtonPanel.repaint();
         scoreButtonPanel.repaint();
-        scorePanel.repaint();
-        
-        frame.add(scorePanel);
+        rollPanel.repaint();
+        tablePanel.repaint();
+    }
+
+    private void findTotals(){
+        int upperSec = 0, lowerSec = 0;
+        for(int player=1;player<=2;player++){
+            for(int i=1;i<=6;i++)
+                upperSec += Integer.parseInt((String)table.getValueAt(i, player));
+            table.setValueAt("" + upperSec, 7, player);
+            if(upperSec >= 63){
+                table.setValueAt("35", 8, player);
+                upperSec += 35;
+            }
+            else
+                table.setValueAt("0", 8, player);
+            table.setValueAt("" + upperSec, 18, player);
+            for(int i=11;i<=17;i++)
+                lowerSec += Integer.parseInt((String)table.getValueAt(i, player));
+            table.setValueAt("" + lowerSec, 19, player);
+            table.setValueAt("" + (lowerSec+upperSec), 20, player);
+        }
+        update();
     }
 }
