60b49020696bd7aa3a3ff15b260952998d4b1cf8
BenKelly99 <benjaminkelly@me.com>
2015-05-18 16:00:02 -0400

    Created a new class: PlayerCard
    Allows for an OOP solution to having multiple sets of buttons. Technically, this solution would allow for more than 2 players if the main GUI had an array of PlayerCard. Might be implemented in the future

---
+++ source/PlayerCard.java
@@ -0,0 +1,66 @@
+
+/**
+ * Write a description of class PlayerCard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import javax.swing.JComponent;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+public class PlayerCard
+{
+    private JComponent[] scoreButtons;
+    private int catsLeft;
+    
+    public PlayerCard(){
+        catsLeft = 13;
+        setUpButtons();
+    }
+    
+    public PlayerCard(String name){
+        catsLeft = 13;
+        setUpButtons();
+    }
+    
+    private void setUpButtons(){
+        scoreButtons = new JComponent[13];
+        
+        scoreButtons[0] = new JButton("Aces");
+        scoreButtons[1] = new JButton("Twos");
+        scoreButtons[2] = new JButton("Threes");
+        scoreButtons[3] = new JButton("Fours");
+        scoreButtons[4] = new JButton("Fives");
+        scoreButtons[5] = new JButton("Sixes");
+        scoreButtons[6] = new JButton("3 of a Kind");
+        scoreButtons[7] = new JButton("4 of a Kind");
+        scoreButtons[8] = new JButton("Full House");
+        scoreButtons[9] = new JButton("Sm. Straight");
+        scoreButtons[10] = new JButton("Lg. Straight");
+        scoreButtons[11] = new JButton("Yahtzee!");
+        scoreButtons[12] = new JButton("Chance");
+    }
+    
+    public int indexOfButton(JButton button){
+        for(int i=0; i<scoreButtons.length;i++)
+        if(button == scoreButtons[i])
+        return i;
+        return -1;
+    }
+    
+    public void removeButton(int index){
+        scoreButtons[index] = new JLabel();
+        catsLeft--;
+    }
+    
+    //@Pre: 0 <= index < scoreButtons.length
+    public JComponent getComponentAtIndex(int index){
+        return scoreButtons[index];
+    }
+    
+    public boolean isDone(){
+        if(catsLeft <= 0)
+        return true;
+        return false;
+    }
+}
