e53564ab6cc24fd06f8398c4e66d2ac16947f1fb
BenKelly99 <benjaminkelly@me.com>
2015-05-21 12:55:36 -0400

    Update Points.java

--- source/Points.java
+++ source/Points.java
@@ -1,77 +1,121 @@
-
-/**
- * Write a description of class PlayerCard here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+/*
+ * File: Points.java
+ * Name: Giacalone/Kelly/Xue
+ * Date: 05/10/2015
+ * -------------------------
+ * This takes an array od dice values and the clicked button, and returns the
+ * correct number of points.
+ * NOTE: Author of the selectionSort method: Lewis/Loftus/Cocking
  */
-import javax.swing.JComponent;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-public class PlayerCard
+
+import javax.swing.*;
+
+public class Points
 {
-    protected JComponent[] scoreButtons;
-    private int catsLeft;
-    private String name;
-    
-    public PlayerCard(){
-        catsLeft = 13;
-        name = "Player";
-        setUpButtons();
+    public static int getPoints(JButton button, int[] arr) {
+        int[] sort = arr;
+        sort = selectionSort(sort);
+            
+        int total = 0;
+
+        if(button.getText().equals("Aces")) { // total = sum of the ones
+            for(int i: sort)
+                if(i == 1) total += 1;
+        }
+        else if(button.getText().equals("Twos")) { // total = sum of the twos
+            for(int i: sort)
+                if(i == 2) total += 2;
+        }
+        else if(button.getText().equals("Threes")) { // total = sum of the threes
+            for(int i: sort)
+                if(i == 3) total += 3;
+        }
+        else if(button.getText().equals("Fours")) { // total = sum of the fours
+            for(int i: sort)
+                if(i == 4) total += 4;
+        }
+        else if(button.getText().equals("Fives")) { // total = sum of the fives
+            for(int i: sort)
+                if(i == 5) total += 5;
+        }
+        else if(button.getText().equals("Sixes")) { // total = sum of the sixes
+            for(int i: sort)
+                if(i == 6) total += 6;
+        }
+        else if(button.getText().equals("3 of a Kind")) {
+            if((sort[0] == sort[1] && sort[1] == sort[2])
+                || (sort[1] == sort[2] && sort[2] == sort[3])
+                || (sort[2] == sort[3] && sort[3] == sort[4])) {
+                for(int i: sort)
+                    total += i;
+            }
+        }
+        else if(button.getText().equals("4 of a Kind")) {
+            if((sort[0] == sort[1] && sort[1] == sort[2] && sort[2] == sort[3])
+                || (sort[1] == sort[2] && sort[2] == sort[3] && sort[3] == sort[4])) {
+                for(int i: sort)
+                    total += i;
+            }
+        }
+        else if(button.getText().equals("Full House")) {
+            if(sort[0] == sort[1] && sort[2] == sort[3] && (sort[3] == sort[4]))
+                total = 25;
+            else if(sort[0] == sort[1] && sort[1] == sort[2] && sort[3] == sort[4])
+                total = 25;
+        }
+        else if(button.getText().equals("Sm. Straight")) {
+            total = 30;
+            for(int i = 1; i < sort.length - 1; i++)
+                if(sort[i] != sort[i - 1] + 1)
+                    total = 0;
+            if(total == 0) {
+                total = 30;
+                for(int i = 2; i < sort.length; i++)
+                if(sort[i] != sort[i - 1] + 1)
+                    total = 0;
+            }
+            
+        }
+        else if(button.getText().equals("Lg. Straight")) {
+            total = 40;
+            for(int i = 1; i < sort.length; i++)
+                if(sort[i] != sort[i - 1] + 1)
+                    total = 0;
+        }
+        else if(button.getText().equals("Chance")) {
+            for(int i: sort)
+                total += i;
+        }
+        else if(button.getText().equals("Yahtzee!")) {
+            if(sort[0] == sort[1] && sort[1] == sort[2] && sort[2] == sort[3] && sort[3] == sort[4])
+                return 50;
+        }
+
+        return total;
     }
-    
-    public PlayerCard(String name){
-        catsLeft = 13;
-        this.name = name;
-        setUpButtons();
-    }
-    
-    private void setUpButtons(){
-        scoreButtons = new JComponent[13];
+
+    //-----------------------------------------------------------------
+    //  Author of this method: Lewis/Loftus/Cocking
+    //  Sorts the specified array of integers using the selection
+    //  sort algorithm.
+    //-----------------------------------------------------------------
+    public static int[] selectionSort (int[] numbers)
+    {
+        int min, temp;
+
+        for (int index = 0; index < numbers.length-1; index++)
+        {
+            min = index;
+            for (int scan = index+1; scan < numbers.length; scan++)
+                if (numbers[scan] < numbers[min])
+                    min = scan;
+
+            // Swap the values
+            temp = numbers[min];
+            numbers[min] = numbers[index];
+            numbers[index] = temp;
+        }
         
-        scoreButtons[0] = new JButton("Aces");
-        scoreButtons[1] = new JButton("Twos");
-        scoreButtons[2] = new JButton("Threes");
-        scoreButtons[3] = new JButton("Fours");
-        scoreButtons[4] = new JButton("Fives");
-        scoreButtons[5] = new JButton("Sixes");
-        scoreButtons[6] = new JButton("3 of a Kind");
-        scoreButtons[7] = new JButton("4 of a Kind");
-        scoreButtons[8] = new JButton("Full House");
-        scoreButtons[9] = new JButton("Sm. Straight");
-        scoreButtons[10] = new JButton("Lg. Straight");
-        scoreButtons[11] = new JButton("Yahtzee!");
-        scoreButtons[12] = new JButton("Chance");
-    }
-    
-    public int indexOfButton(JButton button){
-        for(int i=0; i<scoreButtons.length;i++)
-        if(button == scoreButtons[i])
-        return i;
-        return -1;
-    }
-    
-    public void removeButton(int index){
-        scoreButtons[index] = new JLabel();
-        catsLeft--;
-    }
-    
-    //@Pre: 0 <= index < scoreButtons.length
-    public JComponent getComponentAtIndex(int index){
-        return scoreButtons[index];
-    }
-    
-    public boolean isDone(){
-        if(catsLeft <= 0)
-        return true;
-        return false;
-    }
-    
-    public String getName(){
-        return name;
-    }
-    
-    public void setName(String name){
-        this.name = name;
+        return numbers;
     }
 }
